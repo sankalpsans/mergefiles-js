ab4fd880a9cce0d1ef8cd34f1d01d10806cf91d4
Sankalp <sankalp.sans@gmail.com>
2016-06-28 14:10:28 +0530

    Sync read, write operations + logging
    * Critical for consistency.
    * Console output of files being read and written to

--- bin/mergefiles.js
+++ bin/mergefiles.js
@@ -3,17 +3,11 @@ module.exports = {
  mergeJob: function(mergeFiles) {
     var components = mergeFiles['sourceFiles'];
     var output = mergeFiles['outputFile'];
-    fs.writeFile(output, '/* Generated merged file: '+ (new Date()).toString() +' */' + "\n", function (err) {
-        if (err) {
-            return console.log(err);
-        }
-    });
+    fs.writeFileSync(output, '/* Generated merged file: '+ (new Date()).toString() +' */' + "\n");
+    headingLog('Building bundle ' + output);
     for (var i = 0; i < components.length; i = i + 1) {
-        fs.appendFile(output, fs.readFileSync(components[i]).toString(), function (err, data) {
-            if (err) {
-                return console.log(err);
-            }
-        });
+        console.log('New file: '+components[i]);
+        fs.appendFileSync(output, fs.readFileSync(components[i]).toString());
     }
  }
 };
@@ -27,3 +21,10 @@ var read = function(file) {
         return data;
     });
 };
+
+/**
+ * Used to log an action in Sky blue color so that it stands out
+ */
+function headingLog(string) {
+        console.log('\n\n\033[36m' + string + '\033[0m');
+}
